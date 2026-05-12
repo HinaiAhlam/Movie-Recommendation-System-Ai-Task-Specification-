1524183b5c7e777ba5cf1fe95e04fd337bc11e8d
“karim <moza.m@gmail.com>
2026-05-12 16:35:39 +0400

    Search System

---
+++ Movie Recommendation System Ai Task Specification/project/Interface/ISearch.cs
@@ -0,0 +1,17 @@
+﻿using project.Models;
+using System.Collections.Generic;
+using project.Models;
+
+namespace YourProject.Interfaces
+{
+    public interface ISearch
+    {
+        List<Movie> SearchByTitle(string title);
+        List<Movie> SearchByGenre(string genre);
+        List<Movie> SearchByYear(int year);
+        List<Movie> SearchByDirector(string director);
+        List<Movie> SearchByRating(double rating);
+
+        List<Movie> SmartSearch(string keyword);
+    }
+}
